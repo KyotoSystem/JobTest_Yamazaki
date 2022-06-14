fdf2d4148da5a4e15f798d860cbcee16e8822e45
KyotoSystemYamazaki <yamazaki.kyosysgi@gmail.com>
2022-06-14 12:06:20 +0900

    山﨑var1.06

---
+++ EntryTestCs/Model/Firstnumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntryTestCs.Model
+{
+    public class Firstnumber
+    {
+        #region コンストラクター
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        /// <param name="fnum">第一数値</param>
+        public Firstnumber(string fnum)
+        {
+            Fnum = fnum;
+        }
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// 第一数値
+        /// </summary>
+        public string Fnum { get; set; }
+        #endregion
+
+    }
+}
