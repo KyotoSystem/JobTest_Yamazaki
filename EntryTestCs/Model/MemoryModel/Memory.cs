dfaf98597ca4ff7fcb22eb792a97f7efbba9923b
KyotoSystemYamazaki <yamazaki.kyosysgi@gmail.com>
2022-06-14 17:19:11 +0900

    山﨑var1.10

---
+++ EntryTestCs/Model/MemoryModel/Memory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntryTestCs.Model.MemoryModel
+{
+    public class Memory
+    {
+        #region コンストラクター
+        /// <summary>
+        /// コンストラクター
+        /// </summary>
+        /// <param name="Mnum">メモリ数値</param>
+        public Memory(double Mnum)
+        {
+            MNum = Mnum;
+        }
+        #endregion
+
+        #region プロパティ
+        /// <summary>
+        /// メモリ数値
+        /// </summary>
+        public double MNum { get; set; }
+        #endregion
+    }
+}
