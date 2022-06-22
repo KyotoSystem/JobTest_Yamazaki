6e553df55376ecf3195fd2a3ff461da3b6f09726
KyotoSystemYamazaki <yamazaki.kyosysgi@gmail.com>
2022-06-22 17:40:59 +0900

    Seekvar1.0

--- EntryTestCs/Model/PlayerModel/Player.cs
+++ EntryTestCs/Model/PlayerModel/Player.cs
@@ -7,28 +7,6 @@ namespace EntryTestCs.Model.PlayerModel
 {
     public class Player
     {
-        #region コンストラクター
-        /// <summary>
-        /// コンストラクター
-        /// </summary>
-        /// <param name="x">X座標</param>
-        /// <param name="y">Y座標</param>
-        public Player(int x , int y)
-        {
-            X = x;
-            Y = y;
-        }
-        #endregion
-
-        #region プロパティ
-        /// <summary>
-        /// X座標
-        /// </summary>
-    　　public int X { get; set; }
-        /// <summary>
-        /// Y座標
-        /// </summary>
-        public int Y { get; set; }
-        #endregion
+        
     }
 }
