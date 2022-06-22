fc6232a3544cabe97ced1697b6bbec310c176f4f
KyotoSystemYamazaki <yamazaki.kyosysgi@gmail.com>
2022-06-22 13:14:53 +0900

    game var1.03

--- EntryTestCs/Contller/PlayerContller.cs
+++ EntryTestCs/Contller/PlayerContller.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using EntryTestCs.Model.PlayerModel;
 
 namespace EntryTestCs.Contller
 {
     class PlayerContller
     {
+        public Player GetPlayer()
+        {
+            Player player = null;
+            return player;
+        }
     }
 }
